8251bc741dd9474659c56439395ecf30e1b14b92
Oren Dayan <orendayan@Orens-MacBook-Pro.local>
2017-08-20 02:56:07 +0300

    Added Nanit assets to the project.

---
+++ app/src/main/java/com/orend/happybirthday/fragments/BirthdayFragment.java
@@ -0,0 +1,11 @@
+package com.orend.happybirthday.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by orendayan on 20/08/2017.
+ */
+
+public class BirthdayFragment extends Fragment {
+
+}
